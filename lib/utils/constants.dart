a0f9988fbb7b0142072899756744af690e3d6eec
Quốc Diễn <quoc.dien@mitek.vn>
2024-08-29 18:04:48 +0700

    change path

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -7,5 +7,5 @@ class MTNetworkConstant {
   static const String url = "https://videocall-api-vt.mitek.vn/api/v1/integration/";
   static const String authenticate = "/getConfigVideoCall";
   static const String createConnection = "/createConnection";
-  static const String initVideoCall = "/createConnection";
+  static const String createRoom = "/createRoom";
 }
